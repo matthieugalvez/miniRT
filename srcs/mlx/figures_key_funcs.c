b6d23d516a8ca74439bf8647b639e9a7d35b09b0
Anne-Sophie Chantraine <achantra@paul-f5Ar3s3.clusters.42paris.fr>
2025-02-24 17:51:37 +0100

    figures

--- srcs/mlx/figures_key_funcs.c
+++ srcs/mlx/figures_key_funcs.c
@@ -3,19 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   figures_key_funcs.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgalvez <mgalvez@student.42.fr>            +#+  +:+       +#+        */
+/*   By: achantra <achantra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 17:24:45 by mgalvez           #+#    #+#             */
-/*   Updated: 2025/02/24 17:26:45 by mgalvez          ###   ########.fr       */
+/*   Updated: 2025/02/24 17:46:32 by achantra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-void	ft_rotate_figure(int keysym, t_env *env, t_element *figure);
+void	ft_rotate_figure(int keysym, t_env *env, t_element *figure)
+{
+    
+}
 
-void	ft_elevate_figure(int keysym, t_env *env, t_element *figure);
+void	ft_elevate_figure(int keysym, t_env *env, t_element *figure)
+{
+    
+}
 
-void	ft_translate_figure(int keysym, t_env *env, t_element *figure);
+void	ft_translate_figure(int keysym, t_env *env, t_element *figure)
+{
+    
+}
 
-void	ft_scale(int keysym, t_env *env, t_element *figure);
+void	ft_scale(int keysym, t_env *env, t_element *figure)
+{
+    
+}
