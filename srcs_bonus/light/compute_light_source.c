04671e494ed8e10487ce5e35ea986bb730816ef0
Matthieu Galvez <mgalvez@paul-f4Br5s4.clusters.42paris.fr>
2025-02-26 14:55:34 +0100

    debug specular light

--- srcs_bonus/light/compute_light_source.c
+++ srcs_bonus/light/compute_light_source.c
@@ -6,22 +6,11 @@
 /*   By: mgalvez <mgalvez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 10:59:44 by mgalvez           #+#    #+#             */
-/*   Updated: 2025/02/26 11:00:26 by mgalvez          ###   ########.fr       */
+/*   Updated: 2025/02/26 14:52:14 by mgalvez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT_bonus.h"
-
-static t_coordinates	get_reflexion_vec(t_coordinates *light_ray,
-							t_coordinates *normal_at_hp)
-{
-	t_coordinates	factor;
-	double			cos_angle;
-
-	cos_angle = scalar_prod_vec(*normal_at_hp, *light_ray);
-	factor = mult_vec(mult_vec(*normal_at_hp, 2), cos_angle);
-	return (sub_vec(*light_ray, factor));
-}
 
 static void	get_specular(t_coordinates reflexion_vec, t_ray *cam_ray,
 						t_light *light, t_color *color)
@@ -30,15 +19,31 @@ static void	get_specular(t_coordinates reflexion_vec, t_ray *cam_ray,
 	double	factor;
 	double	specular;
 
-	cos_angle = scalar_prod_vec(reflexion_vec, mult_vec(*cam_ray->direction,
-				-1));
+	cos_angle = scalar_prod_vec(reflexion_vec, *cam_ray->direction);
 	if (cos_angle <= 0)
 		return ;
-	factor = pow(cos_angle, 200);
-	specular = light->bright * 0.075 * factor;
+	factor = pow(cos_angle, 250);
+	specular = light->bright * 0.15 * factor;
 	color->r += light->color->r * specular;
 	color->g += light->color->g * specular;
 	color->b += light->color->b * specular;
+	if (color->r > 255)
+		color->r = 255;
+	if (color->g > 255)
+		color->g = 255;
+	if (color->b > 255)
+		color->b = 255;
+}
+
+static t_coordinates	get_reflexion_vec(t_coordinates *light_ray,
+							t_coordinates *normal_at_hp)
+{
+	t_coordinates	factor;
+	double			cos_angle;
+
+	cos_angle = 2 * scalar_prod_vec(*normal_at_hp, *light_ray);
+	factor = mult_vec(*normal_at_hp, cos_angle);
+	return (sub_vec(factor, *light_ray));
 }
 
 static void	get_diffuse(t_light *light, t_color *color, t_color *hitpoint_color,
@@ -59,6 +64,12 @@ static void	get_diffuse(t_light *light, t_color *color, t_color *hitpoint_color,
 	color->r += applied_diffuse.r;
 	color->g += applied_diffuse.g;
 	color->b += applied_diffuse.b;
+	if (color->r > 255)
+		color->r = 255;
+	if (color->g > 255)
+		color->g = 255;
+	if (color->b > 255)
+		color->b = 255;
 }
 
 static int	find_shadow(t_env *env, t_element *current_figure, t_ray *light_ray)
@@ -97,15 +108,11 @@ void	compute_light_source(t_env *env, t_hitpoint *hitpoint,
 	cos_angle = scalar_prod_vec(normal_at_hp, mult_vec(*light_ray.direction,
 				-1));
 	if (!find_shadow(env, figure, &light_ray) && cos_angle >= 0)
+	{
 		get_diffuse(env->light, color, hitpoint->color, cos_angle);
-	reflexion_vec = get_reflexion_vec(light_ray.direction, &normal_at_hp);
-	normalize_vec(&reflexion_vec);
-	get_specular(reflexion_vec, env->camera->ray, env->light, color);
-	if (color->r > 255)
-		color->r = 255;
-	if (color->g > 255)
-		color->g = 255;
-	if (color->b > 255)
-		color->b = 255;
+		reflexion_vec = get_reflexion_vec(light_ray.direction, &normal_at_hp);
+		normalize_vec(&reflexion_vec);
+		get_specular(reflexion_vec, env->camera->ray, env->light, color);
+	}
 	free(light_ray.direction);
 }
