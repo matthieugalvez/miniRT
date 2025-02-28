2f45f7c72a8c33055881eaffc13eba4a5dfbb620
Anne-Sophie Chantraine <achantra@paul-f4Br4s4.clusters.42paris.fr>
2025-02-28 10:01:36 +0100

    checkerboard plane

--- srcs_bonus/image/check_color.c
+++ srcs_bonus/image/check_color.c
@@ -6,7 +6,7 @@
 /*   By: achantra <achantra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 13:59:16 by achantra          #+#    #+#             */
-/*   Updated: 2025/02/26 17:53:13 by mgalvez          ###   ########.fr       */
+/*   Updated: 2025/02/28 10:01:19 by achantra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@ static int	is_odd_sp(t_hitpoint *hitpoint, t_element *figure)
 	double	v;
 	int		x_value;
 	int		y_value;
-	int		z_value;
 
-	u = 10
+	u = 100
 		* (atan2(hitpoint->coord->y, hitpoint->coord->x) + M_PI) / (2 * M_PI);
-	v = 10 * (acos(2 * hitpoint->coord->z / figure->diameter) + M_PI_2) / M_PI;
+	v = 100 * (acos(2 * hitpoint->coord->z / figure->diameter) + M_PI_2) / M_PI;
+	
 	x_value = (int) u % 2;
 	y_value = (int) v % 2;
-	z_value = (int) hitpoint->coord->z % 2;
-	return ((x_value + y_value + z_value) % 2);
+	return ((x_value + y_value) % 2);
 }
 
 static int	is_odd_cy(t_hitpoint *hitpoint, t_element *figure)
@@ -45,17 +44,17 @@ static int	is_odd_cy(t_hitpoint *hitpoint, t_element *figure)
 	{
 		u = hitpoint->coord->x + hitpoint->coord->y;
 		v = hitpoint->coord->z;
-		x_value = (int) hitpoint->coord->x % 2;
-		y_value = (int) hitpoint->coord->y % 2;
-		//z_value = (int) hitpoint->coord->z % 2;
+		x_value = (int)hitpoint->coord->x % 2;
+		y_value = (int)hitpoint->coord->y % 2;
+		// z_value = (int) hitpoint->coord->z % 2;
 	}
 	else
 	{
 		u = 100 * (theta + M_PI) / (2 * M_PI);
 		v = 100 * z / figure->height;
-		x_value = (int) u % 2;
-		y_value = (int) v % 2;
-		//z_value = (int) hitpoint->coord->z % 2;
+		x_value = (int)u % 2;
+		y_value = (int)v % 2;
+		// z_value = (int) hitpoint->coord->z % 2;
 		/*u = hitpoint->coord->x + hitpoint->coord->y;
 		v = hitpoint->coord->z;
 		x_value = (int) hitpoint->coord->x % 2;
@@ -65,12 +64,63 @@ static int	is_odd_cy(t_hitpoint *hitpoint, t_element *figure)
 	return ((x_value + y_value) % 2);
 }
 
+static int	is_odd_co(t_hitpoint *hitpoint, t_element *figure)
+{
+	double	theta;
+	double	z;
+	double	u;
+	double	v;
+	int		x_value;
+	int		y_value;
+
+	theta = atan2(hitpoint->coord->y, hitpoint->coord->x);
+	z = hitpoint->coord->z;
+	if (hitpoint->coord->z == figure->coord->z + figure->height)
+	{
+		u = hitpoint->coord->x + hitpoint->coord->y;
+		v = hitpoint->coord->z;
+		x_value = (int)hitpoint->coord->x % 2;
+		y_value = (int)hitpoint->coord->y % 2;
+		// z_value = (int) hitpoint->coord->z % 2;
+	}
+	else
+	{
+		u = 100 * (theta + M_PI) / (2 * M_PI);
+		v = 100 * z / figure->height;
+		x_value = (int)u % 2;
+		y_value = (int)v % 2;
+		// z_value = (int) hitpoint->coord->z % 2;
+		/*u = hitpoint->coord->x + hitpoint->coord->y;
+		v = hitpoint->coord->z;
+		x_value = (int) hitpoint->coord->x % 2;
+		y_value = (int) hitpoint->coord->y % 2;
+		z_value = (int) hitpoint->coord->z % 2;*/
+	}
+	return ((x_value + y_value) % 2);
+}
+
+static int	is_odd_pl(t_hitpoint *hitpoint, t_element *figure)
+{
+	int	x_value;
+	int	y_value;
+	int	z_value;
+
+	x_value = (int)floor(hitpoint->coord->x) % 2;
+	y_value = (int)floor(hitpoint->coord->y) % 2;
+	z_value = (int)floor(hitpoint->coord->z) % 2;
+	return ((x_value + y_value + z_value) % 2);
+}
+
 static int	is_odd_coord(t_hitpoint *hitpoint, t_element *figure)
 {
 	if (figure->id == SPHERE)
 		return (is_odd_sp(hitpoint, figure));
 	else if (figure->id == CYLINDER)
 		return (is_odd_cy(hitpoint, figure));
+	else if (figure->id == PLANE)
+		return (is_odd_pl(hitpoint, figure));
+	else if (figure->id == CONE)
+		return (is_odd_co(hitpoint, figure));
 	return (1);
 }
 
