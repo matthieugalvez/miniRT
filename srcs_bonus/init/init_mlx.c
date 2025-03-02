d95adb6a222248b9d6b7df413aef52bb2862d6a0
Matthieu Galvez <mgalvez@paul-f4Ar8s6.clusters.42paris.fr>
2025-03-02 20:35:12 +0100

    better plane texture rendition and mlx leak fix

--- srcs_bonus/init/init_mlx.c
+++ srcs_bonus/init/init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: mgalvez <mgalvez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 13:14:57 by mgalvez           #+#    #+#             */
-/*   Updated: 2025/02/25 18:01:03 by mgalvez          ###   ########.fr       */
+/*   Updated: 2025/03/02 20:31:37 by mgalvez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,6 @@
 
 int	init_mlx(t_env *env)
 {
-	env->mlx = mlx_init();
-	if (!env->mlx)
-	{
-		ft_putstr("Error: mlx\n", 2);
-		return (clean_env(env, 1));
-	}
 	env->win = mlx_new_window(env->mlx, WIN_W, WIN_H, "miniRT");
 	if (!env->win)
 	{
