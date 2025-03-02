9d408f96e489563494ff1e03a5b380eb09b70f4f
Anne-Sophie Chantraine <achantra@paul-f4Ar7s9.clusters.42paris.fr>
2025-03-02 19:16:38 +0100

    camdir

--- srcs_bonus/init/init_utils2.c
+++ srcs_bonus/init/init_utils2.c
@@ -6,7 +6,7 @@
 /*   By: achantra <achantra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 11:30:55 by achantra          #+#    #+#             */
-/*   Updated: 2025/03/01 16:41:52 by mgalvez          ###   ########.fr       */
+/*   Updated: 2025/03/02 18:57:19 by achantra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ t_coordinates	find_tmp_up(t_coordinates *vector)
 {
 	t_coordinates	tmp_up;
 
-	if (!equal_double(vector->y, 0) || !equal_double(vector->z, 0))
+	if (equal_double(vector->x, 0))
 	{
-		if (vector->y < 0 || vector->z < 0)
+		if (vector->z < 0)
 			tmp_up = change_vec(-1, 0, 0);
 		else
 			tmp_up = change_vec(1, 0, 0);
@@ -35,9 +35,9 @@ t_coordinates	find_tmp_up(t_coordinates *vector)
 	else
 	{
 		if (vector->x < 0)
-			tmp_up = change_vec(0, -1, 0);
+			tmp_up = change_vec(0, 0, 1);
 		else
-			tmp_up = change_vec(0, 1, 0);
+			tmp_up = change_vec(0,0,-1);
 	}
 	return (tmp_up);
 }
