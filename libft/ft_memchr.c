0e1faed11834d602f1161886c83e31177351c06b
Matthieu GALVEZ <mgalvez@student.42.fr>
2026-05-30 22:04:37 +0200

    remove unused file

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -18,7 +18,7 @@ void *ft_memchr(const void *s, int c, size_t n) {
     i = 0;
     while (i < n) {
         if (((unsigned char *)s)[i] == (unsigned char)c)
-            return ((void *)&s[i]);
+            return (&((unsigned char *)s)[i]);
         i++;
     }
     return (NULL);
